5ae86c215f33eb67f13aee0a20ac8d889e520b43
Walid Mahmoud - Tchisuky <mwalid.mahmoud@gmail.com>
2018-11-22 19:59:36 +0100

    :newpapers: creating User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,12 +1,79 @@
 <?php
+
+namespace src\Entity;
+
+use app\Entity;
+
 /**
- * Created by PhpStorm.
- * User: walid
- * Date: 11/21/2018
- * Time: 12:43 AM
+ * Class User
+ * @package src\Entity
  */
-
-class User
+class User extends Entity
 {
+    /**
+     * @var int clé du user
+     */
+    private $id;
+    /**
+     * @var string pseudo, login etc
+     */
+    private $username;
+    /**
+     * @var string mdp
+     */
+    private $password;
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param $id
+     * @return User
+     */
+    public function setId($id): User
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param $username
+     * @return User
+     */
+    public function setUsername($username): User
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param $password
+     * @return User
+     */
+    public function setPassword($password): User
+    {
+        $this->password = $password;
+        return $this;
+    }
 }
