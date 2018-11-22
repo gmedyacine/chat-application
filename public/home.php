b3e9488b213d8aafbb1290f1baac2d15dc2e0590
Walid Mahmoud - Tchisuky <mwalid.mahmoud@gmail.com>
2018-11-23 00:08:39 +0100

    :lipstick: bootsrapping

--- public/home.php
+++ public/home.php
@@ -1,16 +1,20 @@
 <h1>Bienvenue sur votre application de chat</h1>
-<a href="/?p=chat.logout">Déconnection</a>
+<a class="btn btn-primary" href="/?p=chat.logout" role="button">Déconnexion</a>
 
 <h2>Liste des utilisateurs connectés</h2>
-<ul id="users"></ul>
+<ul class="list-group" id="users"></ul>
 
 <h2>Messages</h2>
-<ul id="messages"></ul>
+<ul class="list-group" id="messages"></ul>
 
 <form method="post">
-    <textarea rows="4" name="content"></textarea>
+    <div class="form-group">
+        <label for="username"></label>
+        <textarea class="form-control" rows="5" name="content" id="content" required
+                  placeholder="Votre message"></textarea>
+    </div>
     <br/>
-    <input type="submit" name="Envoyer">
+    <button type="submit" class="btn btn-primary">Envoyer</button>
 </form>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -33,7 +37,7 @@
                 success: function (data) {
                     $("#messages").empty();
                     for (message in data) {
-                        $("#messages").append('<li><strong>' + data[message].username + '</strong> <i>[' + data[message].createdAt + ']</i> :' + data[message].content + '</li>');
+                        $("#messages").append('<li class="list-group-item">><strong>' + data[message].username + '</strong> <i>[' + data[message].createdAt + ']</i> :' + data[message].content + '</li>');
                     }
                 }
             });
@@ -46,7 +50,7 @@
                 success: function (data) {
                     $("#users").empty();
                     for (message in data) {
-                        $("#users").append('<li>' + data[message] + '</li>');
+                        $("#users").append('<li class="list-group-item">>' + data[message] + '</li>');
                     }
                 }
             });
--- public/login.php
+++ public/login.php
@@ -1,31 +1,58 @@
 <h1>Connexion</h1>
 
 <?php
-    if (isset($loginError)):
-?>
-<p class="error"><?= $loginError; ?>
+if (isset($loginError) && $loginError !== ''):
+    ?>
+    <div class="alert alert-danger" role="alert">
+        <?php
+        echo $loginError;
+        ?>
+    </div>
 <?php
-    endif
+endif
 ?>
 
 <form method="post">
-    <input type="text" name="username" required="true">
-    <input type="password" name="password" required="true">
-    <input type="submit" name="login" value="Se connecter">
+    <div class="form-group">
+        <label for="username">Nom d'utilisateur</label>
+        <input type="text" class="form-control" name="username" id="username" required
+               placeholder="Entrez votre nom d'utilisateur">
+    </div>
+    <div class="form-group">
+        <label for="password">Mots de passe</label>
+        <input type="password" class="form-control" name="password" required id="password"
+               placeholder="Votre mots de passe">
+    </div>
+    <button type="submit" name="login" class="btn btn-primary">Se connecter</button>
 </form>
 
 <h2>S'enregistrer</h2>
-<?php if (isset($registrationError)): ?>
-<p class="error">
+<?php if (isset($registrationError)  && $registrationError !== ''): ?>
+    <div class="alert alert-danger" role="alert">
+        <?php
+        echo $registrationError;
+        ?>
+    </div>
+
 <?php
-        $registrationError;
-?>
-<?php
-    endif
+endif
 ?>
 <form method="post">
-    <input type="text" name="username" required="true">
-    <input type="password" name="password" required="true">
-    <input type="password" name="password2" required="true">
-    <input type="submit" name="register" value="Inscription">
+    <div class="form-group">
+        <label for="username">Nom d'utilisateur</label>
+        <input type="text" class="form-control" name="username" id="username" required aria-describedby="usernameHelp"
+               placeholder="Entrez votre nom d'utilisateur">
+        <small id="usernameHelp" class="form-text text-muted">Veuillez choisir un nom d'utilisateur respectueux</small>
+    </div>
+    <div class="form-group">
+        <label for="password">Mots de passe</label>
+        <input type="password" class="form-control" name="password" required id="password"
+               placeholder="Votre mots de passe">
+    </div>
+    <div class="form-group">
+        <label for="password">Confirmation du mots de passe</label>
+        <input type="password" class="form-control" name="password2" required id="password2"
+               placeholder="Réentrez votre mots de passe">
+    </div>
+    <button type="submit" name="register" class="btn btn-primary">Inscription</button>
 </form>
