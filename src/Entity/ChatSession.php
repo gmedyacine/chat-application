bf5a688a28014942bd2c5dbcbeacd49de59249f7
Walid Mahmoud - Tchisuky <mwalid.mahmoud@gmail.com>
2018-11-22 19:59:06 +0100

    :newspaper: ChatSession entity creation

--- src/Entity/ChatSession.php
+++ src/Entity/ChatSession.php
@@ -1,15 +1,85 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: walid
- * Date: 11/21/2018
- * Time: 12:51 AM
- */
 
 namespace src\Entity;
 
 
-class chatSession
-{
+use app\Entity;
+use src\Entity\User;
+use src\Manager\ChatSessionManager;
 
+/**
+ * Class ChatSession
+ * @package src\Entity
+ */
+class ChatSession extends Entity
+{
+    /**
+     * @var int clé
+     */
+    private $id;
+    /**
+     * @var User La classe User
+     */
+    private $user;
+    /**
+     * @var \DateTime de création de l'entité
+     */
+    private $createdAt;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param $id
+     * @return ChatSession
+     */
+    public function setId($id): ChatSession
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return \src\Entity\User
+     */
+    public function getUser():User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     * @return ChatSession
+     */
+    public function setUser(User $user): ChatSession
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param $createdAt
+     * @return ChatSession
+     */
+    public function setCreatedAt($createdAt): ChatSession
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
 }
