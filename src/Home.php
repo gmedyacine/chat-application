84051f7a5806d9c459ffaa6c3aaed52612d35d34
Walid Mahmoud - Tchisuky <mwalid.mahmoud@gmail.com>
2018-11-22 20:02:14 +0100

    :pencil: autloader + db instances

--- src/Home.php
+++ src/Home.php
@@ -8,39 +8,64 @@ use app\Database\MysqlDatabase;
 /*
  * Class principale qui permet de charger une instance de l'autoloader et de la connexion à la base de donnée
  */
-class App
-{
-    private static $_instance;
-    private $_dbInstance;
 
-    public static function getInstance(){
-        if(is_null(self::$_instance)){
-            self::$_instance = new App();
+/**
+ * Class Home
+ * @package src
+ */
+class Home
+{
+    /**
+     * @var $instance une instance de l'autolaoder
+     */
+    private static $instance;
+    /**
+     * @var $dbInstance une instance de la bd
+     */
+    private $dbInstance;
+
+    /**
+     * @return Home
+     */
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new Home();
         }
-        return self::$_instance;
+        return self::$instance;
     }
 
+    /**
+     *
+     */
     public static function load()
     {
-        if(!isset($_SESSION)) {
+        if (!isset($_SESSION)) {
             session_start();
         }
         // chargement des autoloaders
-        require APPPATH .'/Autoloader.php';
+        require APPPATH . '/Autoloader.php';
         Autoloader::register();
         require ROOT . '/app/Autoloader.php';
         \app\Autoloader::register();
     }
 
+    /**
+     * @return MysqlDatabase
+     */
     public function getDb()
     {
         // récupération des paramètres de connexion
         $config = Config::getInstance(ROOT . '/config/config.php');
 
         // instanciation de la BDD si elle n'existe pas
-        if(is_null($this->_dbInstance)) {
-            $this->_dbInstance = new MysqlDatabase($config->get('db_name'), $config->get('db_user'), $config->get('db_pass'), $config->get('db_host'));
+        if (is_null($this->dbInstance)) {
+            $this->dbInstance = new MysqlDatabase($config->get('database'),
+                $config->get('user'),
+                $config->get('password'),
+                $config->get('host')
+            );
         }
-        return $this->_dbInstance;
+        return $this->dbInstance;
     }
 }
