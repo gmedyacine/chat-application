a1b0bfa12685e367189ce1d9dd51a3c8765362f4
Walid Mahmoud - Tchisuky <mwalid.mahmoud@gmail.com>
2018-11-23 10:43:04 +0100

    :newspaper: :sparkles: new token class
    Pour palier aux failles CSRF

---
+++ src/Security/Token.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace src\Security;
+
+class Token
+{
+    public static function generate()
+    {
+        return $_SESSION['token'] = md5($_SESSION['token'] ?? time() * rand(60, 122));
+    }
+
+    public static function check($token): bool
+    {
+        if(isset($_SESSION['token']) && $token === $_SESSION['token']){
+            unset($_SESSION['token']);
+            return true;
+        }
+        return false;
+    }
+}
