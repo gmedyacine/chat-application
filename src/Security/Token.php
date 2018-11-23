140439b956435edeba0ada82e8d234e6787c5f58
Walid Mahmoud - Tchisuky <mwalid.mahmoud@gmail.com>
2018-11-23 10:55:05 +0100

    :books: commenting token class

--- src/Security/Token.php
+++ src/Security/Token.php
@@ -2,13 +2,24 @@
 
 namespace src\Security;
 
+/**
+ * Class Token
+ * @package src\Security
+ */
 class Token
 {
+    /**
+     * @return string
+     */
     public static function generate()
     {
         return $_SESSION['token'] = md5($_SESSION['token'] ?? time() * rand(60, 122));
     }
 
+    /**
+     * @param $token
+     * @return bool
+     */
     public static function check($token): bool
     {
         if(isset($_SESSION['token']) && $token === $_SESSION['token']){
